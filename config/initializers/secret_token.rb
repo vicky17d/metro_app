1995c330b65ced104fa6015080843032d78597c6
VickyD <vicky17d@gmail.com>
2013-06-08 21:33:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetroApp::Application.config.secret_token = 'da8732491e17960e9049e1c37e6d919eeaa37a953864d4a066dfc7ab8df52d73eb2ccc1aa479711a5e854b2d38b198fcb7f5711eff66194a78d1c793b6633956'
